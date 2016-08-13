3f858287af7a36d93cd7797ad857aeec5abdd7ae
Moe Odeh <m.odeh93@gmail.com>
2016-08-13 01:53:29 -0400

    version 0.3

--- infoLCD.ino
+++ infoLCD.ino
@@ -1,3 +1,44 @@
+/* ***************************************************************
+ *  DESCRIPTION:
+ *   - A very basic program that displays local weather information
+ *     such as (temp, humidity, heat index).
+ *  
+ *  AUTHOR:
+ *   - Mohammad Odeh
+ *  
+ *  VERSION:
+ *   - 0.3
+ * ****************************************************************
+ *  
+ * LCD SETUP:
+ *  VSS -> GND
+ *  VDD -> +5V
+ *  VO  -> Middle pin of potentiometer
+ *  RS  -> PIN 7
+ *  RW  -> GND
+ *  E   -> PIN 8
+ *  D4  -> PIN 9
+ *  D5  -> PIN 10
+ *  D6  -> PIN 11
+ *  D7  -> PIN 12
+ *  A   -> 220ohm -> PIN 4
+ *  K   -> GND
+ * 
+ * WAKE UP PUSHBUTTON SETUP:
+ *  Left pin  -> PIN 3
+ *  Right pin -> GND
+ * 
+ * TEMP. + HUMIDITY SENSOR SETUP:
+ *  +   -> VIN
+ *  out -> PIN 2 (Also jump a 1Kohm from + to out)
+ *  -   -> GND
+ *  
+ * POTENTIOMETER SEUP:
+ *  Left pin  -> VIN
+ *  Right Pin -> GND
+ */
+
+// Include needed libraries
 #include <LowPower.h>
 #include <DHT.h>
 #include <LiquidCrystal.h>
@@ -9,7 +50,7 @@
 #define button 3
 #define displaySwitch 4
 
-// LiquidCrystal(rs, enable, d4, d5, d6, d7)
+// LiquidCrystal name(rs, enable, d4, d5, d6, d7)
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -27,54 +68,83 @@ void setup() {
 
   // Print greeting message
   lcd.print("Hello, Moe.");
-  delay(1000);
+  delay(1500);
+  lcd.clear();
 }
 
 void loop() {
-  lcd.clear();
+  // Display message
+  lcd.print("Going to sleep..");
+  lcd.setCursor(0, 1);
   lcd.print("Display OFF");
-  delay(1000);
+  delay(1500);
+  // Turn OFF display
   lcd.noDisplay();
+  lcd.clear();
   digitalWrite(displaySwitch, LOW);
   // Create an interrupt service routine (ISR)
+  /* Here the interrupt cue is attached to the pin we specify; we tell the atmega to
+  // watch for the specified change to occur (changing the pin's state from whatever to LOW)
+  // and when that happens call the function (displayON)
+  attachInterrupt(digitalPinToInterrupt(2or3), functionToCall, stateChange); */
   attachInterrupt(digitalPinToInterrupt(3), displayON, LOW);
+  // Add delay() for stability
+  delay(50);
+  // powerDown the atmega
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  // After interrupt is triggered and displayON is called the MC will continue code execution here
   detachInterrupt(digitalPinToInterrupt(3));
 }
 
 void displayON() {
-  digitalWrite(displaySwitch, HIGH);
-  lcd.clear();
-  lcd.display();
-  delay(1000);
+
+  // Do readings first as this sensor is cheap and slow
   // Read humidity level
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float c = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(c, h, false);
+
+  // Turn ON and display message
   lcd.print("Display ON");
-  delay(1000);
+  lcd.display();
+  digitalWrite(displaySwitch, HIGH);
+  delay(1500);
   lcd.clear();
+
+  // Print greeting message
+  lcd.print("Hello, Moe.");
+  delay(1500);
+  lcd.clear();
+
   //1st column, 1st row
   lcd.setCursor(0, 0);
   // print float with 1 decimal precision
   lcd.print(c, 1);
   lcd.print("C");
-  delay(500);
+  delay(750);
 
-  // 6th column, 1st row
-  lcd.setCursor(7, 0);
-  lcd.print(h, 1);
-  lcd.print("Humid");
-  delay(500);
+  // 5th column, 1st row
+  lcd.setCursor(6, 0);
+  lcd.print(f, 1);
+  lcd.print("F");
+  delay(750);
 
   // 1st column, 2nd row
   lcd.setCursor(0, 1);
+  lcd.print(h, 1);
+  lcd.print("Hmd");
+  delay(750);
+
+  // 11th column, 2nd row
+  lcd.setCursor(8, 1);
   lcd.print(hic, 1);
-  lcd.print(" H.I.");
+  lcd.print("HI");
 
   // Delay for stability
-  delay(5000);
+  delay(2500);
   lcd.clear();
 }
